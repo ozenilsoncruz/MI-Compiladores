d863033d4e76702a22eaf7b88a24c0fbe3ce0742
andersonlima7 <andersonlll007@gmail.com>
2023-08-23 23:03:55 -0300

    Criado lógica para pegar o número da linha

---
+++ config.py
@@ -0,0 +1,66 @@
+palavras_reservadas = {
+    "variables",
+    "const",
+    "class",
+    "methods",
+    "objects",
+    "main",
+    "return",
+    "if",
+    "else",
+    "then",
+    "for",
+    "read",
+    "print",
+    "void",
+    "int",
+    "real",
+    "boolean",
+    "string",
+    "true",
+    "false",
+}
+
+# expressões regulares da estrutura léxica da linguagem.
+estrutura_lexica = {
+    "identificador": r"^[a-zA-Z][a-zA-Z0-9_]*$",
+    "numero": r"^[0-9]+(\.[0-9]+)?$",
+    "cadeia_caracteres": r'^".*"?$',
+    "delimitadores": [";", ",", ".", "(", ")", "[", "]", "{", "}", "->"],
+    "operadores_relacionais": ["!=", "==", "<", "<=", ">", ">=", "="],
+    "operadores_aritmeticos": ["+", "-", "*", "/", "++", "--"],
+    "operadores_logicos": ["!", "&&", "||"],
+}
+
+
+#  Código identificadores dos tokens
+""" 
+PRE palavra reservada
+IDE identificador
+CAC cadeia de caracteres
+NRO numero
+DEL delimitador 
+REL operador relacional
+LOG operador logico
+ART operador aritmético
+CMF cadeia mal formada
+CoMF comentário mal formado
+NMF numero mal formado
+IMF identificador mal formado
+TMF	token mal formado
+"""
+codigos = {
+    "PRE",
+    "IDE",
+    "CAC",
+    "NRO",
+    "DEL",
+    "REL",
+    "LOG",
+    "ART",
+    "CMF",
+    "COMF",
+    "NMF",
+    "IMF",
+    "TMF",
+}
--- main.py
+++ main.py
@@ -1,61 +1,59 @@
 import re, os
+from config import palavras_reservadas, estrutura_lexica, codigos
 
 
-def analisa_lexema(texto: list, dict_dados: dict):
-    
+def analisa_lexema(linha: list, num_linha: int, tokens_saida: list):
     texto_saida = ""
-
-    for n_linha, linha in enumerate(texto):
-        for palavra in linha.split():
-            caractere = 0
-            # verifica se começa com " para se encaixar em cadeia de caracteres
-            if re.match(r'^"', palavra) is not None:
-                if re.match(dict_dados.get('cadeia_caracteres'), palavra) is not None:
-                    print('Cadeia de caracteres')
-                else:
-                    print('Erro na cadeia de caracteres')
-            elif re.match(r'^[0-9]', palavra) is not None:
-                if re.match(dict_dados.get('numero'), palavra) is not None:
-                    print('Numero')
-                else:
-                    print('Erro no numero')
-            elif re.match(r'^[a-zA-Z]', palavra) is not None:
-
-                if re.match(dict_dados.get('identificador'), palavra) is not None:
-                    print('Identificador')
-                else:
-                    print('Erro no identificador')
+    for palavra in linha:
+        caractere = 0
+        # verifica se começa com " para se encaixar em cadeia de caracteres
+        if re.match(r'^"', palavra) is not None:
+            if re.match(estrutura_lexica.get("cadeia_caracteres"), palavra) is not None:
+                token = f"{num_linha} {codigos.get('cac')}"
+                print("Cadeia de caracteres")
+            else:
+                print("Erro na cadeia de caracteres")
+        elif re.match(r"^[0-9]", palavra) is not None:
+            if re.match(estrutura_lexica.get("numero"), palavra) is not None:
+                print("Numero")
+            else:
+                print("Erro no numero")
+        elif re.match(r"^[a-zA-Z]", palavra) is not None:
+            if re.match(estrutura_lexica.get("identificador"), palavra) is not None:
+                print("Identificador")
+            else:
+                print(f"Erro no identificador ")
 
 
-reservadas = {
-    'variables', 'const', 'class', 'methods',
-    'objects', 'main', 'return', 'if', 'else',
-    'then', 'for', 'read', 'print', 'void', 'int',
-    'real', 'boolean', 'string', 'true', 'false'
-}
-
-
-# seu tipo de dado e a expressoes regular equivaalente
-tipo_dado = {
-    'identificador': r'^[a-zA-Z][a-zA-Z0-9_]*$',
-    'numero': r'^[0-9]+(\.[0-9]+)?$',
-    'cadeia_caracteres': r'^".*"?$',
-    'delimitadores': [';', ',', '.', '(', ')', '[', ']', '{', '}', '->'],
-    'operadores_relacionados': ['!=', '==', '<', '<=', '>', '>=', '='],
-    'operadores_aritmeticos': ['+', '-', '*', '/', '++', '--'],
-    'operadores_logicos': ['!', '&&', '||']
-}
-
-
-pasta = '/content/files'
-arquivos = os.listdir(pasta)
-
-for arquivo in arquivos:
+# Salva as palavras lidas em um dicionário do tipo {número da linha: [palavras, da, linha]}
+def ler_arquivo(pasta, arquivo):
+    palavras_entrada = {}
     # Verifique se é realmente um arquivo
     if os.path.isfile(os.path.join(pasta, arquivo)):
-        with open(os.path.join(pasta, arquivo), 'r') as a:
-            texto = [string for string in a.read().split('\n') if string != ""]
-            analisa_lexema(texto, tipo_dado)
+        with open(os.path.join(pasta, arquivo), "r") as a:
+            # Divida o aquivo em linhas.
+            linhas = a.read().split("\n")
+            # Divisão do conteúdo em palavras, considerando espaços e tabulações como separadores.
+            for num_linha, linha in enumerate(linhas):
+                if linha.strip():  # Verifica se a linha não é vazia
+                    num_linha += 1
+                    palavras = linha.split()
+                    if num_linha not in palavras_entrada:
+                        palavras_entrada[num_linha] = []
+                    palavras_entrada[num_linha].extend(palavras)
+    return palavras_entrada
 
 
+def main():
+    pasta = "./files"
+    arquivos = os.listdir(pasta)
+    tokens_saida = []
+    for arquivo in arquivos:
+        palavras_entrada = ler_arquivo(pasta, arquivo)
+        for num_linha, palavras in palavras_entrada.items():
+            print(palavras)
+            # analisa_lexema(palavras, num_linha, tokens_saida)
 
+
+if __name__ == "__main__":
+    main()
